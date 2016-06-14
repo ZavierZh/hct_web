024e9f11597d6a53a9ac3a38c5828b14ae998dff
zhouwei <zhouwei@hct.sh.cn>
2016-06-14 14:26:58 +0800

    m:小修改

--- HCT/Common/Conf/config.php
+++ HCT/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 		'DB_HOST'               =>  'localhost', // 服务器地址
 //		'DB_NAME'               =>  'project',          // 数据库名
 		'DB_USER'               =>  'root',      // 用户名
-		'DB_PWD'                =>  'zz',          // 密码
+		'DB_PWD'                =>  'zhouwei',          // 密码
 		'DB_NAME' => 'hct_manage',
 		'DB_PREFIX' => 'hct_',
 		//thinkphp的调试工具
